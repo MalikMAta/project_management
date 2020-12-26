378fca815c9d73091137221e0f1b19771d3fe0e7
Malik Ata <Malikmajedata@gmail.com>
2020-12-26 00:13:47 -0800

    DashBoard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import {BrowserRouter} from 'react-router-dom'
-import NavBar from './componets/layout/NavBar'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./componets/dashboard/Dashboard";
+import NavBar from "./componets/layout/NavBar";
+import DashBoard from "./componets/dashboard/Dashboard";
 
 function App() {
   return (
-
     <BrowserRouter>
-        <div className="App">
-      <NavBar />
-      <Switch>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          {/* Create the routes for each component */}
 
-        {/* Create the routes for each component */}
-      </Switch>
-        </div>
-  </BrowserRouter>
+          <Route to="/" component={Dashboard} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/componets/projects/ProjectList.js
+++ src/componets/projects/ProjectList.js
@@ -1,37 +1,14 @@
-import React from 'react'
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
-const ProjectList=() => {
-    return (
-        <div>
-            <div className="project-list section">
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+    </div>
+  );
+};
 
-        <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by malik</p>
-                <p className="grey-text">3.23.2020</p>
-            </div>
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by malik</p>
-                <p className="grey-text">3.23.2020</p>
-            </div>
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by malik</p>
-                <p className="grey-text">3.23.2020</p>
-            </div>
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by malik</p>
-                <p className="grey-text">3.23.2020</p>
-            </div>
-            
-        </div>
-
-            </div>
-        </div>
-    )
-}
-
-export default ProjectList
+export default ProjectList;
